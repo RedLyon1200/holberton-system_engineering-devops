a8a6815b1a3811021e6c1c250995e3411e202e0b
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-14 20:40:06 +0000

    Task: 2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" -*- Coding UTF-8 -*- """
+import requests
+from sys import argv
+from pprint import pprint
+import json
+
+
+def get_employee(id):
+    """ Get employe by id from API """
+    user_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    ).json()
+
+    name = user_data.get('name')
+    if not name:
+        return
+
+    """Get all the tasks -*- TODOS"""
+    all_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/'
+    ).json()
+
+    """Records all tasks that are owned by this employee"""
+    tasks_user = [task for task in all_tasks if task.get('userId') == id]
+
+    """ Generate JSON data response """
+    response = {str(id): []}
+    for task in tasks_user:
+        data = {
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': name
+        }
+        response[str(id)].append(data)
+
+    """ Save JSON data in a file """
+    with open('{}.json'.format(id), 'w') as json_file:
+        json.dump(response, json_file)
+
+
+if __name__ == "__main__":
+    """ Entry point """
+    get_employee(int(argv[1]))
