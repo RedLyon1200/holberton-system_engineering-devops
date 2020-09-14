30aae00607a3274c5964f0fab96419619c0ec564
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-14 21:08:04 +0000

    params added

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,20 +8,15 @@ import requests
 def get_employee(id):
     """ Get employe by id from API """
     user_data = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+        'https://jsonplaceholder.typicode.com/users/', params={'id': id}
     ).json()
 
-    name = user_data.get('name')
-    if not name:
-        return
-
-    """Get all the tasks -*- TODOS"""
-    all_tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/'
-    ).json()
+    username = user_data[0].get('username')
 
     """Records all tasks that are owned by this employee"""
-    tasks_user = [task for task in all_tasks if task.get('userId') == id]
+    tasks_user = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/', params={'userId': id}
+    ).json()
 
     """ Creates a file with employedd id by file name """
     with open('{}.csv'.format(id), mode='w', encoding='UTF-8') as file:
@@ -33,9 +28,11 @@ def get_employee(id):
         )
 
         for task in tasks_user:
-            data.writerow([id, name, task.get('completed'), task.get('title')])
+            data.writerow([id, username, task.get(
+                'completed'), task.get('title')])
 
 
 if __name__ == "__main__":
     """ Entry point """
-    get_employee(int(argv[1]))
+    if argv[1].isdigit():
+        get_employee(int(argv[1]))
