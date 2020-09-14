3bd9a644e50c841fb695c31c6161ef5c5e9a1aa3
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-14 20:40:06 +0000

    Task: 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" -*- Coding UTF-8 -*- """
 import requests
 from sys import argv
 
@@ -13,17 +13,21 @@ def get_employee(id):
     if not user_data.get('name'):
         return
 
+    """Get all the tasks -*- TODOS"""
     all_tasks = requests.get(
         'https://jsonplaceholder.typicode.com/todos/'
     ).json()
 
+    """Records all tasks that are owned by this employee"""
     tasks_user = [task for task in all_tasks if task.get('userId') == id]
 
+    """Records all tasks this employee has completed"""
     tasks_done = [task for task in tasks_user if task.get('completed')]
 
     response = 'Employee {} is done with tasks({}/{}):'.format(
         user_data.get('name'), len(tasks_done), len(tasks_user)
     )
+
     print(response)
     for task in tasks_done:
         print(task.get('title'))
