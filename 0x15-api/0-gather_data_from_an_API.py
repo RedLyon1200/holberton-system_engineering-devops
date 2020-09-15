96c6496f245880cf6d9f7b79299e07d505822b0d
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-15 16:34:10 +0000

    params added to request

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,24 @@ import requests
 def get_employee(id):
     """ Get employe by id from API """
     user_data = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+        'https://jsonplaceholder.typicode.com/users/', params={'id': id}
     ).json()
 
-    if not user_data.get('name'):
-        return
+    name = user_data[0].get('name')
 
-    """Get all the tasks -*- TODOS"""
-    all_tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/'
+    """Records all tasks that are owned and have completed by this employee"""
+    tasks_done = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/', params={
+            'userId': id, 'completed': 'true'}
     ).json()
 
-    """Records all tasks that are owned by this employee"""
-    tasks_user = [task for task in all_tasks if task.get('userId') == id]
-
-    """Records all tasks this employee has completed"""
-    tasks_done = [task for task in tasks_user if task.get('completed')]
+    tasks_user = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/', params={
+            'userId': id}
+    ).json()
 
     response = 'Employee {} is done with tasks({}/{}):'.format(
-        user_data.get('name'), len(tasks_done), len(tasks_user)
+        name, len(tasks_done), len(tasks_user)
     )
 
     print(response)
