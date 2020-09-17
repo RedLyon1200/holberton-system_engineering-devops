3b81c81bbc3091e1194b9d22c83a186a425fa17b
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-17 06:00:44 +0000

    Task: 1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+from pprint import pprint
+
+
+def top_ten(subreddit):
+    """[summary]
+
+    Args:
+        subreddit ([str]): [subreddit]
+
+    Returns:
+        [int]: [the titles of the first 10 hot posts listed]
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = {'User-Agent': '1639-holbertonschool'}
+    req = requests.get(url, headers=user_agent,
+                       allow_redirects=False, params={'limit': 10})
+    if req.status_code == 200:
+        req = req.json()
+        children = req.get('data').get('children')
+        if children:
+            for post in children:
+                print(post.get('data').get('title'))
+            return
+    print(None)
